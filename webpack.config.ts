5d4802ad09e2ec5407b4886d3f996457808fab2a
ayamoosa <ayamoosa98@gmail.com>
2023-02-27 15:55:35 -0800

    full boilerplate Co-authored-by: Sarah Cynn <cynnsarah@users.noreply.github.com>

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,3 +1,52 @@
 const path = require('path');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: path.resolve(__dirname, './src/index.ts'),
+  module: {
+    rules: [
+      {
+          test: /\.jsx?/,
+          exclude: /node_modules/,
+          use: {
+              loader: 'babel-loader',
+              options: {
+              presets: ['@babel/preset-env', '@babel/preset-react'] 
+              }
+          }
+          },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [ new HtmlWebpackPlugin({ template: './src/index.html' })],
+  mode: process.env.NODE_ENV,
+  devServer: {
+    historyApiFallback: true,
+    static: {
+        directory: path.resolve(__dirname, './client'),
+        publicPath: '/'
+    },
+    port: 8080,
+    compress: true,
+    proxy: {
+        '/': 'http://localhost:3000/',
+    }
+  }, 
+};
+
 
